361f201cde247a13e18400610dbbe291121467b0
Vatsal Yadav <vatsalyad15@gmail.com>
2020-05-17 04:11:10 +0530

    Add News, Source and Article models to store News data
    Added News, Source and Article models to store data in private fields, and to make the data accessible to the rest of the application through setter and getter methods.

---
+++ app/src/main/java/com/vatsalyadav/apps/news/model/News.java
@@ -0,0 +1,33 @@
+package com.vatsalyadav.apps.news.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class News {
+
+    @SerializedName("status")
+    @Expose
+    private String status;
+
+    @SerializedName("articles")
+    @Expose
+    private List<Article> article;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public List<Article> getArticle() {
+        return article;
+    }
+
+    public void setArticle(List<Article> article) {
+        this.article = article;
+    }
+}
