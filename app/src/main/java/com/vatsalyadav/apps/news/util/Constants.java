a5e45c27f8e2ab5e79c039ef1aaf4db200602f84
Vatsal Yadav <vatsalyad15@gmail.com>
2020-05-17 04:06:51 +0530

    Add Base url and failed status Constants

---
+++ app/src/main/java/com/vatsalyadav/apps/news/util/Constants.java
@@ -0,0 +1,6 @@
+package com.vatsalyadav.apps.news.util;
+
+public class Constants {
+    public static final String BASE_URL = "https://candidate-test-data-moengage.s3.amazonaws.com/Android/news-api-feed/staticResponse.json";
+    public static final String STATUS_FAILED = "FAILED";
+}
