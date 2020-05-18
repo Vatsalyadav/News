a315a13f59f3cbbb5cd3cfb532295c4ec449740c
Vatsal Yadav <vatsalyad15@gmail.com>
2020-05-18 18:05:41 +0530

    Add Local Storage Database Helper and Contract
    Add Local Storage Database Helper and Contract to store news in SQLite database and show when there is no internet connectivity.

---
+++ app/src/main/java/com/vatsalyadav/apps/news/repository/localStorageNews/NewsDatabaseHelper.java
@@ -0,0 +1,72 @@
+package com.vatsalyadav.apps.news.repository.localStorageNews;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.google.gson.Gson;
+import com.vatsalyadav.apps.news.model.Article;
+
+import androidx.annotation.Nullable;
+
+public class NewsDatabaseHelper extends SQLiteOpenHelper {
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "NewsReader.db";
+    private static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + NewsReaderContract.NewsEntry.TABLE_NAME + " (" +
+                    NewsReaderContract.NewsEntry._ID + " INTEGER PRIMARY KEY," +
+                    NewsReaderContract.NewsEntry.COLUMN_NAME_ARTICLE_TITLE + " TEXT," +
+                    NewsReaderContract.NewsEntry.COLUMN_NAME_ARTICLE_URL + " TEXT," +
+                    NewsReaderContract.NewsEntry.COLUMN_NAME_ARTICLE_JSON + " TEXT, UNIQUE(" +
+                    NewsReaderContract.NewsEntry.COLUMN_NAME_ARTICLE_TITLE + "," +
+                    NewsReaderContract.NewsEntry.COLUMN_NAME_ARTICLE_URL + "))";
+    private static final String SQL_FETCH_ENTRIES =
+            "SELECT * FROM " + NewsReaderContract.NewsEntry.TABLE_NAME;
+    private static final String SQL_DELETE_TABLE =
+            "DROP TABLE IF EXISTS " + NewsReaderContract.NewsEntry.TABLE_NAME;
+
+    public NewsDatabaseHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(SQL_CREATE_ENTRIES);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL(SQL_DELETE_TABLE);
+    }
+
+    public boolean insertNews(Article newsArticle) {
+        // Gets the data repository in write mode
+        SQLiteDatabase db = this.getWritableDatabase();
+        // Create a new map of values, where column names are the keys
+        ContentValues values = new ContentValues();
+        Gson gson = new Gson();
+        newsArticle.setArticleSaved(true);
+        values.put(NewsReaderContract.NewsEntry.COLUMN_NAME_ARTICLE_TITLE, newsArticle.getTitle());
+        values.put(NewsReaderContract.NewsEntry.COLUMN_NAME_ARTICLE_URL, newsArticle.getUrl());
+        values.put(NewsReaderContract.NewsEntry.COLUMN_NAME_ARTICLE_JSON, gson.toJson(newsArticle));
+        // Insert the new row, returning the primary key value of the new row
+        long newRowId = db.insert(NewsReaderContract.NewsEntry.TABLE_NAME, null, values);
+        db.close();
+        return newRowId != -1;
+    }
+
+    public Cursor getData() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.rawQuery(SQL_FETCH_ENTRIES, null);
+    }
+
+    public int deleteNewsArticle(Article article) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        int delete = db.delete(NewsReaderContract.NewsEntry.TABLE_NAME, NewsReaderContract.NewsEntry.COLUMN_NAME_ARTICLE_URL + "=? and " + NewsReaderContract.NewsEntry.COLUMN_NAME_ARTICLE_TITLE + "=?",
+                new String[]{article.getUrl(), article.getTitle()});
+        db.close();
+        return delete;
+    }
+}
---
+++ app/src/main/java/com/vatsalyadav/apps/news/repository/localStorageNews/NewsReaderContract.java
@@ -0,0 +1,18 @@
+package com.vatsalyadav.apps.news.repository.localStorageNews;
+
+import android.provider.BaseColumns;
+
+public class NewsReaderContract {
+    // To prevent someone from accidentally instantiating the contract class,
+    // make the constructor private.
+    private NewsReaderContract() {
+    }
+
+    /* Inner class that defines the table contents */
+    public static class NewsEntry implements BaseColumns {
+        public static final String TABLE_NAME = "saved_news";
+        public static final String COLUMN_NAME_ARTICLE_JSON = "article_json";
+        public static final String COLUMN_NAME_ARTICLE_TITLE = "article_title";
+        public static final String COLUMN_NAME_ARTICLE_URL = "article_url";
+    }
+}
