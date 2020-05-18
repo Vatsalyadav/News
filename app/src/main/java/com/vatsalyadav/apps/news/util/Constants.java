83241abe25968cec1abd6b79ca58913ed63790c9
Vatsal Yadav <vatsalyad15@gmail.com>
2020-05-18 18:12:59 +0530

    Add DateFormat Util method and Status OK Constant

--- app/src/main/java/com/vatsalyadav/apps/news/util/Constants.java
+++ app/src/main/java/com/vatsalyadav/apps/news/util/Constants.java
@@ -3,4 +3,5 @@ package com.vatsalyadav.apps.news.util;
 public class Constants {
     public static final String BASE_URL = "https://candidate-test-data-moengage.s3.amazonaws.com/Android/news-api-feed/staticResponse.json";
     public static final String STATUS_FAILED = "FAILED";
+    public static final String STATUS_OK = "ok";
 }
---
+++ app/src/main/java/com/vatsalyadav/apps/news/util/Utils.java
@@ -0,0 +1,30 @@
+package com.vatsalyadav.apps.news.util;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class Utils {
+
+    public static String DateFormat(String oldstringDate) {
+        String newDate;
+        SimpleDateFormat dateFormat = new SimpleDateFormat("E, d MMM yyyy", new Locale(getCountry()));
+        try {
+            Date date = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'").parse(oldstringDate);
+            newDate = dateFormat.format(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            newDate = oldstringDate;
+        }
+
+        return newDate;
+    }
+
+    public static String getCountry() {
+        Locale locale = Locale.getDefault();
+        String country = String.valueOf(locale.getCountry());
+        return country.toLowerCase();
+    }
+
+}
