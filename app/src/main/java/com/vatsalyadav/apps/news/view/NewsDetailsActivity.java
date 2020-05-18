b1207d97fbbcbe63e1c8d322c2c2a302766ad24a
Vatsal Yadav <vatsalyad15@gmail.com>
2020-05-18 23:57:34 +0530

    Add NewsDetailsActivity to show selected News

---
+++ app/src/main/java/com/vatsalyadav/apps/news/view/NewsDetailsActivity.java
@@ -0,0 +1,91 @@
+package com.vatsalyadav.apps.news.view;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.vatsalyadav.apps.news.R;
+
+import androidx.appcompat.app.ActionBar;
+import dagger.android.support.DaggerAppCompatActivity;
+
+public class NewsDetailsActivity extends DaggerAppCompatActivity {
+
+    private static final String EXTRA_SELECTED_NEWS_PUBLISHER = "EXTRA_SELECTED_NEWS_PUBLISHER";
+    private static final String EXTRA_NEWS_URL = "EXTRA_NEWS_URL";
+
+    private String selectedNewsTitle;
+    private String newsUrl;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_details);
+        getExtrasFromIntent(getIntent());
+        ActionBar actionBar = getSupportActionBar();
+        setActionBar(actionBar);
+        initWebView(newsUrl);
+    }
+
+    private void getExtrasFromIntent(Intent intent) {
+        selectedNewsTitle = intent.getStringExtra(EXTRA_SELECTED_NEWS_PUBLISHER);
+        newsUrl = intent.getStringExtra(EXTRA_NEWS_URL);
+    }
+
+    // Create intent for NewsDetailsActivity with data to initiate it
+    public Intent launchNewsDetails(Context context, String articleTitle, String articleUrl) {
+        Intent intent = new Intent(context, NewsDetailsActivity.class);
+        intent.putExtra(EXTRA_SELECTED_NEWS_PUBLISHER, articleTitle);
+        intent.putExtra(EXTRA_NEWS_URL, articleUrl);
+        return intent;
+    }
+
+    private void setActionBar(ActionBar actionBar) {
+        if (actionBar != null) {
+            actionBar.setTitle(selectedNewsTitle);
+            actionBar.setSubtitle(newsUrl);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    private void initWebView(String url) {
+        WebView webView = findViewById(R.id.web_view_news_details);
+        webView.getSettings().setLoadsImagesAutomatically(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setSupportZoom(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+        webView.getSettings().setDisplayZoomControls(false);
+        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        webView.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl(url);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        supportFinishAfterTransition();
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
