b6cad042a2915385314b23023f504dc1132d47a3
Vatsal Yadav <vatsalyad15@gmail.com>
2020-05-23 20:49:59 +0530

    Add onBind and onClick No Position check in NewsListAdapter

--- app/src/main/java/com/vatsalyadav/apps/news/adapter/NewsListAdapter.java
+++ app/src/main/java/com/vatsalyadav/apps/news/adapter/NewsListAdapter.java
@@ -51,42 +51,9 @@ public class NewsListAdapter extends RecyclerView.Adapter<NewsListAdapter.ViewHo
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holders, int position) {
-        final ViewHolder holder = holders;
-        Article model;
-        model = articles.get(position);
-
-        RequestOptions requestOptions = new RequestOptions()
-                .placeholder(new ColorDrawable(Color.parseColor("#f9f9fa")))
-                .error(new ColorDrawable(Color.parseColor("#f9f9fa")))
-                .diskCacheStrategy(DiskCacheStrategy.ALL)
-                .centerCrop();
-
-        Glide.with(context)
-                .load(model.getUrlToImage())
-                .apply(requestOptions)
-                .listener(new RequestListener<Drawable>() {
-                    @Override
-                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
-                        holder.progressBar.setVisibility(View.GONE);
-                        return false;
-                    }
-
-                    @Override
-                    public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
-                        holder.progressBar.setVisibility(View.GONE);
-                        return false;
-                    }
-                })
-                .transition(DrawableTransitionOptions.withCrossFade())
-                .into(holder.imageView);
-
-        holder.title.setText(model.getTitle());
-        holder.desc.setText(model.getDescription());
-        holder.source.setText(model.getSource().getName());
-        holder.published_ad.setText(Utils.DateFormat(model.getPublishedAt()));
-        holder.author.setText(model.getAuthor());
-        holder.buttonSave.setIcon(model.getArticleSaved() ? holder.itemView.getResources().getDrawable(R.drawable.ic_love_pink) : holder.itemView.getResources().getDrawable(R.drawable.ic_love_grey));
+    public void onBindViewHolder(@NonNull final ViewHolder holders, int position) {
+        Article article = articles.get(position);
+        holders.bindTo(article);
     }
 
     @Override
@@ -112,7 +79,7 @@ public class NewsListAdapter extends RecyclerView.Adapter<NewsListAdapter.ViewHo
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    if (onNewsClickListener != null) {
+                    if (onNewsClickListener != null && getAdapterPosition() != RecyclerView.NO_POSITION) {
                         onNewsClickListener.onNewsClick(getAdapterPosition());
                     }
                 }
@@ -124,7 +91,7 @@ public class NewsListAdapter extends RecyclerView.Adapter<NewsListAdapter.ViewHo
             source = itemView.findViewById(R.id.source);
             imageView = itemView.findViewById(R.id.img);
             progressBar = itemView.findViewById(R.id.prograss_load_photo);
-            buttonSave = (MaterialButton) itemView.findViewById(R.id.button_save);
+            buttonSave = itemView.findViewById(R.id.button_save);
             buttonSave.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
@@ -133,6 +100,41 @@ public class NewsListAdapter extends RecyclerView.Adapter<NewsListAdapter.ViewHo
                     }
                 }
             });
+        }
+
+        public void bindTo(Article article) {
+
+            RequestOptions requestOptions = new RequestOptions()
+                    .placeholder(new ColorDrawable(Color.parseColor("#f9f9fa")))
+                    .error(new ColorDrawable(Color.parseColor("#f9f9fa")))
+                    .diskCacheStrategy(DiskCacheStrategy.ALL)
+                    .centerCrop();
+
+            Glide.with(context)
+                    .load(article.getUrlToImage())
+                    .apply(requestOptions)
+                    .listener(new RequestListener<Drawable>() {
+                        @Override
+                        public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                            progressBar.setVisibility(View.GONE);
+                            return false;
+                        }
+
+                        @Override
+                        public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                            progressBar.setVisibility(View.GONE);
+                            return false;
+                        }
+                    })
+                    .transition(DrawableTransitionOptions.withCrossFade())
+                    .into(imageView);
+
+            title.setText(article.getTitle());
+            desc.setText(article.getDescription());
+            source.setText(article.getSource().getName());
+            published_ad.setText(Utils.DateFormat(article.getPublishedAt()));
+            author.setText(article.getAuthor());
+            buttonSave.setIcon(article.getArticleSaved() ? itemView.getResources().getDrawable(R.drawable.ic_love_pink) : itemView.getResources().getDrawable(R.drawable.ic_love_grey));
         }
 
     }
